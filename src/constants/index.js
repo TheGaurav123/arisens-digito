a9be3a36d8a8017fdd009e77f2521a56b41f73b9
TheGaurav123 <iamakashgaurav321@gmail.com>
2023-12-02 00:40:33 +0530

    Try: Deployment Fix

--- src/constants/index.js
+++
@@ -1,9 +0,0 @@
-import { Service_Data } from "./Service_Data.constants";
-import { SERVICES } from "./Services.constants";
-import { TESTIMONIALS } from "./testimonial.constants";
-
-export {
-    Service_Data,
-    SERVICES,
-    TESTIMONIALS
-}
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,95 @@
 import React from 'react'
 import { Button, Header } from '../components'
 import { Globe, Arrow, DownArrow, DoubleArrowRight, Location, Dialer, Mail, GoogleMap, Logo, Partners, Blog, ServiceVector } from '../assets/images'
-import { Service_Data, SERVICES, TESTIMONIALS } from '../constants'
 import StarImage from '../assets/images/Star.svg'
 import { BrowserRouter } from 'react-router-dom'
+import Image from '../assets/images/Testimonial.svg'
+
+const Service_Data = [
+    {
+        id: 1,
+        label: '101',
+        text: 'Success in getting happy customer'
+    },
+    {
+        id: 2,
+        label: '51',
+        text: 'Thousands of successful business'
+    },
+    {
+        id: 3,
+        label: '500+',
+        text: 'Total clients who loves Arisens Digito'
+    },
+    {
+        id: 4,
+        label: '25',
+        text: 'Stars reviews given by satisfied clients'
+    },
+]
+
+const SERVICES = [
+    {
+        id: 1,
+        label: 'BRANDING',
+        text: 'We take your brand from zero to hero. Our branding workshops provide clarity about your vision, purpose and positioning, along with essentials like tone of voice and customer journey.',
+        css: 'bg-primary'
+    },
+    {
+        id: 2,
+        label: 'CREATIVE',
+        text: "Make people notice with creative and copy that’s uniquely you. Our designers and copywriters capture your style with content that stands out and stops the scroll.",
+        css: 'bg-white text-black'
+    },
+    {
+        id: 3,
+        label: 'WEBSITE',
+        text: "Want a website that just works? We improve the look, feel and experience of your website by running audits that identify how it could be even better.",
+        css: 'bg-primary'
+    },
+    {
+        id: 4,
+        label: 'PAID',
+        text: "We’re paid ads experts. Reach new customers and drive conversions with paid social, PPC and Google ads – we’ll figure out what works best for you!",
+        css: 'bg-white text-black'
+    },
+    {
+        id: 5,
+        label: 'CONTENT',
+        text: "Keep people coming back to your brand with fresh content. Our content strategies keep consumers interested with targeted emails, organic social and SEO-optimised blogs.",
+        css: 'bg-primary'
+    },
+    {
+        id: 6,
+        label: 'INFLUENCER',
+        text: "Engage your audience with people they already know and love. We work with brand-appropriate influencers to create exciting, trustworthy content that’ll put your brand on the digital map.",
+        css: 'bg-white text-black'
+    },
+]
+
+const TESTIMONIALS = [
+    {
+        id: 1,
+        name: 'Jene Mordan',
+        designation: 'Senior Head',
+        comment: "We’re paid ads experts. Reach new customers and drive conversions with paid social, PPC and Google ads – we’ll figure out what works best for you!",
+        avatar: Image
+    },
+    {
+        id: 2,
+        name: 'Jene Mordan',
+        designation: 'Senior Head',
+        comment: "We’re paid ads experts. Reach new customers and drive conversions with paid social, PPC and Google ads – we’ll figure out what works best for you!",
+        avatar: Image
+    },
+    {
+        id: 3,
+        name: 'Jene Mordan',
+        designation: 'Senior Head',
+        comment: "We’re paid ads experts. Reach new customers and drive conversions with paid social, PPC and Google ads – we’ll figure out what works best for you!",
+        avatar: Image
+    },
+]
 
 const Layout = () => {
     return (
